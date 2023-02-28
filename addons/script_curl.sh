0aca857a5cc762c3f0a3e77a9ffd218d7a5c4c6c
thibob <barrethibault@live.fr>
2023-02-28 07:00:28 +0100

    First commit

---
+++ addons/script_curl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -o ChernobyChapterOne.vpk https://steamusercontent-a.akamaihd.net/ugc/780747930176202593/27ADC82BB3F10CC1B2BD7F6980DEEA5F6ACBF501/| curl -o ChernobylPart6.vpk https://steamusercontent-a.akamaihd.net/ugc/760474756246208487/BEFA15F27A07379371E4D55F81BA6250F16FF8AD/
