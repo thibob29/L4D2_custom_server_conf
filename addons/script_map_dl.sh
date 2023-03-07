a66b680146c29a3605c4e1e2b1c4357e6a2758d4
thibob <barrethibault@live.fr>
2023-03-07 05:10:09 +0100

    New sourcemod addon, addons/script_map_dl.sh enhanced.

--- addons/script_map_dl.sh
+++ addons/script_map_dl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-while IFS=, read name link
- do wget $name -O $link 
+while IFS=, read link name
+ do wget $link -O $name 
 done < url.txt #input file, name space link
+echo "Custom maps: " > ../motd.txt | ls |grep .vpk |awk -F '.vpk' '{print $1}' >> ../motd.txt #Write installed custom maps in motd.txt, removing .vpk
